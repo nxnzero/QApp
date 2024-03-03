8e6aaa3193a0d8443b9b42c9a7a8132e89d8ad3e
arch <ereinnchenasky@gmail.com>
2024-03-03 19:26:09 +0700

    Обновил комменты

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import sys
 
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
 
+'''
+QApplication - обработчик приложения
+QWidget - базовый пустой виджет GUI
+Widget - элемент для построения интерфейса(ов)
+'''
+
 application = QApplication(sys.argv) # sys.argv используется для аргументов ком строки
 
 window = QWidget()
